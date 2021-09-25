00db9f71627e64c8f677294fee3dc46ce5edfba8
razan25 <razanAL1234o@gamil.com>
2021-09-25 22:01:45 +0300

    ass2

---
+++ app/src/main/java/com/example/as2/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.as2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+EditText editTextName;
+Button btnClickHere;
+TextView textName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editTextName=(EditText)findViewById(R.id.editTextName);
+        btnClickHere=(Button)findViewById(R.id.btnClickHere);
+        textName=(TextView)findViewById(R.id.textName);
+        btnClickHere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name=editTextName.getText().toString();
+                textName.setText("Hello "+name);
+            }
+        });
+    }
+}
